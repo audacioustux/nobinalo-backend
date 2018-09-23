0e0f6913d1753a820408a1130c5465f3a0ae87f0
Tanjim Hossain <tangimhossain1@gmail.com>
2018-09-23 11:32:16 +0600

    fix schema

--- nobinalo/schema.py
+++ nobinalo/schema.py
@@ -1,15 +1,16 @@
 import graphene
 from django.conf import settings
 from graphene_django.debug import DjangoDebug
+import user.schema
 
 
-class Query(graphene.ObjectType):
+class Query(user.schema.Query, graphene.ObjectType):
     if settings.DEBUG:
         debug = graphene.Field(DjangoDebug, name='__debug')
 
 
-class Mutation(graphene.ObjectType):
+class Mutation(user.schema.Mutation, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- user/schema.py
+++ user/schema.py
@@ -4,3 +4,40 @@ from graphene_django import DjangoObjectType
 
 User = get_user_model()
 
+
+class UserNode(DjangoObjectType):
+    class Meta:
+        model = User
+        interfaces = (graphene.relay.Node,)
+
+
+class CreateUser(graphene.relay.ClientIDMutation):
+    user = graphene.Field(UserNode)
+
+    class Input:
+        email = graphene.String(required=True)
+        full_name = graphene.String(required=True)
+        password = graphene.String(required=True)
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **input):
+        user = User.objects.create_user(
+            email=input.get('email'),
+            full_name=input.get('full_name'),
+            password=input.get('password')
+        )
+        return CreateUser(user=user)
+
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
+
+
+class Query(graphene.ObjectType):
+    me = graphene.Field(UserNode)
+
+    def resolve_me(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('Not logged!')
+        return user
